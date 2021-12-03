e918f14c4f8f94cf339021ab4dff204cb2143628
mallah-elmehdi <elmehdimallah@gmail.com>
2021-12-03 13:21:25 +0100

    lets do

--- DAY02/ex02/main.cpp
+++ DAY02/ex02/main.cpp
@@ -10,5 +10,9 @@ int main(void) {
 	std::cout << a << std::endl;
 	std::cout << b << std::endl;
 	std::cout << Fixed::max( a, b ) << std::endl;
+	std::cout << "/ : " << Fixed(25.65168f) / Fixed(0.6685f) << std::endl;
+	std::cout << "* : " << Fixed(2.5f) * Fixed(5) << std::endl;
+	std::cout << "+ : " << Fixed(33.6518f) + Fixed(1.555f) << std::endl;
+	std::cout << "- : " << Fixed(25.65168f) - Fixed(25.65168f) << std::endl;
 	return 0;
 }
