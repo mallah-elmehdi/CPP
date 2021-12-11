36e5ebdad10deb00b2794874c927a819cb6aa6a5
mallah-elmehdi <elmehdimallah@gmail.com>
2021-12-11 05:55:03 +0100

    [DAY07] : ex00 is done

--- DAY07/ex00/main.cpp
+++ DAY07/ex00/main.cpp
@@ -3,6 +3,24 @@
 #include "swap.hpp"
 #include <iostream>
 
+class Awesome
+{
+	public:
+		Awesome(void) : _n(0) {}
+		Awesome( int n ) : _n( n ) {}
+		Awesome & operator= (Awesome & a) { _n = a._n; return *this; }
+		bool operator==( Awesome const & rhs ) const { return (this->_n == rhs._n); }
+		bool operator!=( Awesome const & rhs ) const{ return (this->_n != rhs._n); }
+		bool operator>( Awesome const & rhs ) const { return (this->_n > rhs._n); }
+		bool operator<( Awesome const & rhs ) const { return (this->_n < rhs._n); }
+		bool operator>=( Awesome const & rhs ) const { return (this->_n >= rhs._n); }
+		bool operator<=( Awesome const & rhs ) const { return (this->_n <= rhs._n); }
+		int get_n() const { return _n; }
+	private:
+		int _n;
+};
+std::ostream & operator<<(std::ostream & o, const Awesome &a) { o << a.get_n(); return o; }
+
 int main(void) {
 	std::cout << "*************************** INT ***************************" << std::endl;
 	int ia = 2;
@@ -27,5 +45,12 @@ int main(void) {
 	std::cout << "fa = " << fa << ", fb = " << fb << std::endl;
 	std::cout << "min(fa, fb) = " << ::min(fa, fb) << std::endl;
 	std::cout << "max(fa, fb) = " << ::max(fa, fb) << std::endl;
+
+	std::cout << "*************************** CLASS *************************" << std::endl;
+	Awesome a(2), b(4);
+	swap(&a, &b);
+	std::cout << a << " " << b << std::endl;
+	std::cout << max(a, b) << std::endl;
+	std::cout << min(a, b) << std::endl;
 	return 0;
 }
